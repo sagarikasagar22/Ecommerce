f894a6f97b5d8fa349460bf4d95226ce59b4724d
Sagarika Sagar <sagarika.sagar@hcl.com>
2021-08-18 19:16:11 +0530

    Registration+Login+List Page design

--- Ecommerce/Controllers/AccountController.cs
+++ Ecommerce/Controllers/AccountController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Ecommerce.Models;
+using Ecommerce.Models.Custom;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,14 +11,74 @@ namespace Ecommerce.Controllers
     public class AccountController : Controller
     {
         // GET: Account
+        private OnlineStoreDataEntities _context = new OnlineStoreDataEntities();
+        // GET: Account
         public ActionResult Registration()
         {
-            return View();
+            return View(new User());
         }
 
+
+
+        [HttpPost]
+        public ActionResult Registration(User user)
+        {
+
+            if (ModelState.IsValid)
+            {
+                var res = _context.Users.Where(x => x.Email == user.Email && x.MobileNumber == user.MobileNumber).FirstOrDefault();
+                if (res != null)
+                {
+                    TempData["ErrorMessage"] = "Either Email or Contact already existed";
+                }
+                else
+                {
+                    _context.Users.Add(user);
+                    var result = _context.SaveChanges();
+                    if (result >= 1)
+                    {
+                        TempData["Message"] = "Successfully Registration Done";
+                    }
+                    else
+                    {
+                        TempData["ErrorMessage"] = "Please COntact the Admin,Might facing some Internal Issues";
+                        return View(user);
+                    }
+
+                    return View(user);
+                }
+            }
+            return View(user);
+        }
+
+
+
+        [HttpPost]
+        public ActionResult Login(CustomLogIn customLogIn)
+        {
+            if (ModelState.IsValid)
+            {
+                var res = _context.Users.Where(x => (x.Email == customLogIn.UserName || x.MobileNumber == customLogIn.UserName) && (x.Password == customLogIn.Password)).FirstOrDefault();
+                if (res != null)
+                {
+                    return RedirectToAction("Index", "Home", new { area = "" });
+                }
+                else
+                {
+                    TempData["Message"] = "Invalid Credentials !";
+                }
+
+
+
+            }
+            return View(customLogIn);
+        }
+        // GET: Login
         public ActionResult Login()
         {
-            return View();
+            return View(new CustomLogIn());
         }
+
+
     }
 }
---
+++ Ecommerce/Models/Custom/CustomLogIn.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Ecommerce.Models.Custom
+{
+    public class CustomLogIn
+    {
+        [Required(ErrorMessage = "Please Enter UserName")]
+        [StringLength(30, MinimumLength = 3)]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Password")]
+        [StringLength(50, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+    }
+}
